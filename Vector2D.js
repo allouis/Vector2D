2a86f8fb224c92e58adc77f37b54b716fba10ce4
Fabien O'Carroll <fabien@Fabiens-Mac-mini.local>
2013-11-06 14:26:06 +0000

    Adding parseArgs function

--- Vector2D.js
+++ Vector2D.js
@@ -1,14 +1,21 @@
 module.exports = Vector2D;
 
-function Vector2D(){
-    var args = Array.apply([], arguments), data;
+function parseArgs(args){
+    var args = Array.apply([], args), data, obj = {};
     if( (data = args, args.length === 2) || (data = args[0], args[0].constructor === Array)){
-        this.x = data[0];
-        this.y = data[1];
+        obj.x = data[0];
+        obj.y = data[1];
     } else {
-        this.x = args[0].x;
-        this.y = args[0].y
+        obj.x = args[0].x;
+        obj.y = args[0].y
     }
+    return obj
+}
+
+function Vector2D(){
+    var args = parseArgs(arguments);
+    this.x = args.x;
+    this.y = args.y;
 }
 
 Vector2D.prototype = {
