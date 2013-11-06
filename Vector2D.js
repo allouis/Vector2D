982c77665c09c099433d1fab033863d8237118a9
Fabien O'Carroll <fabien@Fabiens-Mac-mini.local>
2013-11-06 14:28:32 +0000

    Using parseArgs in module form

--- Vector2D.js
+++ Vector2D.js
@@ -1,16 +1,6 @@
 module.exports = Vector2D;
 
-function parseArgs(args){
-    var args = Array.apply([], args), data, obj = {};
-    if( (data = args, args.length === 2) || (data = args[0], args[0].constructor === Array)){
-        obj.x = data[0];
-        obj.y = data[1];
-    } else {
-        obj.x = args[0].x;
-        obj.y = args[0].y
-    }
-    return obj
-}
+var parseArgs = require("./lib/parseArgs");
 
 function Vector2D(){
     var args = parseArgs(arguments);
@@ -24,4 +14,4 @@ Vector2D.prototype = {
         return new Vector2D(this);
     }
 
-}
+};
