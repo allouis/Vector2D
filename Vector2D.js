27dc3db20f7e22d29a7dbecf43443c21b8549173
Fabien O'Carroll <fabien@Fabiens-Mac-mini.local>
2013-11-06 15:45:35 +0000

    Adding add method

--- Vector2D.js
+++ Vector2D.js
@@ -12,6 +12,13 @@ Vector2D.prototype = {
 
     clone: function(){
         return new Vector2D(this);
+    },
+
+    add: function(){
+        var args = parseArgs(arguments);
+        this.x += args.x;
+        this.y += args.y;
+        return this;
     }
 
 };
